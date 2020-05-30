2db67907755924c5a7d71aeb63931f2c2179d77f
andresperezfc <felipe3apc@gmail.com>
2020-05-30 17:51:50 -0500

    integracion con firebase, funcion guardar añadida

--- app/src/main/java/com/example/personas_material/Datos.java
+++ app/src/main/java/com/example/personas_material/Datos.java
@@ -1,13 +1,23 @@
 package com.example.personas_material;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.ArrayList;
 
 public class Datos {
-
+    private static String db = "Personas";
+    private static DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
     private static ArrayList<Persona> personas = new ArrayList<>();
 
+    public static String getId(){
+        return databaseReference.push().getKey();
+    }
+
     public static void guardar(Persona p){
-        personas.add(p);
+        databaseReference.child(db).child(p.getId()).setValue(p);
+        //personas.add(p);
     }
 
     public static void eliminar (Persona p){
